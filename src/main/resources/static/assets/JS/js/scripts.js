988dcc8f931b86f3efc9d07b55f26301dae89ca3
Ekwere Precious Tom <tompreciousekwere@gmail.com>
2022-10-30 17:16:23 +0100

    refactored the code structure

---
+++ src/main/resources/static/assets/JS/js/scripts.js
@@ -0,0 +1,312 @@
+
+(function($) {
+    "use strict";
+
+    /*================================
+    Preloader
+    ==================================*/
+
+    var preloader = $('#preloader');
+    $(window).on('load', function() {
+        setTimeout(function() {
+            preloader.fadeOut('slow', function() { $(this).remove(); });
+        }, 300)
+    });
+
+    /*================================
+    sidebar menu
+    ==================================*/
+     $("#menu").metisMenu();
+
+    /*================================
+    slimscroll activation
+    ==================================*/
+    $('.menu-inner').slimScroll({
+        height: 'auto'
+    });
+    $('.nofity-list').slimScroll({
+        height: '435px'
+    });
+    $('.timeline-area').slimScroll({
+        height: '500px'
+    });
+    $('.recent-activity').slimScroll({
+        height: 'calc(100vh - 114px)'
+    });
+    $('.settings-list').slimScroll({
+        height: 'calc(100vh - 158px)'
+    });
+
+    /*================================
+    stickey Header
+    ==================================*/
+    $(window).on('scroll', function() {
+        var scroll = $(window).scrollTop(),
+            mainHeader = $('#sticky-header'),
+            mainHeaderHeight = mainHeader.innerHeight();
+
+        // console.log(mainHeader.innerHeight());
+        if (scroll > 1) {
+            $("#sticky-header").addClass("sticky-menu");
+        } else {
+            $("#sticky-header").removeClass("sticky-menu");
+        }
+    });
+
+    /*================================
+    form bootstrap validation
+    ==================================*/
+    $('[data-toggle="popover"]').popover()
+
+    /*------------- Start form Validation -------------*/
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+
+    /*================================
+    datatable active
+    ==================================*/
+    if ($('#dataTable').length) {
+        $('#dataTable').DataTable({
+            responsive: true
+        });
+    }
+    if ($('#dataTable2').length) {
+        $('#dataTable2').DataTable({
+            responsive: true
+        });
+    }
+    if ($('#dataTable3').length) {
+        $('#dataTable3').DataTable({
+            responsive: true
+        });
+    }
+
+
+    /*================================
+    Slicknav mobile menu
+    ==================================*/
+    $('ul#nav_menu').slicknav({
+        prependTo: "#mobile_menu"
+    });
+
+    /*================================
+    login form
+    ==================================*/
+    $('.form-gp input').on('focus', function() {
+        $(this).parent('.form-gp').addClass('focused');
+    });
+    $('.form-gp input').on('focusout', function() {
+        if ($(this).val().length === 0) {
+            $(this).parent('.form-gp').removeClass('focused');
+        }
+    });
+
+    /*================================
+    slider-area background setting
+    ==================================*/
+    $('.settings-btn, .offset-close').on('click', function() {
+        $('.offset-area').toggleClass('show_hide');
+        $('.settings-btn').toggleClass('active');
+    });
+
+    /*================================
+    Owl Carousel
+    ==================================*/
+    function slider_area() {
+        var owl = $('.testimonial-carousel').owlCarousel({
+            margin: 50,
+            loop: true,
+            autoplay: false,
+            nav: false,
+            dots: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                450: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                1000: {
+                    items: 2
+                },
+                1360: {
+                    items: 1
+                },
+                1600: {
+                    items: 2
+                }
+            }
+        });
+    }
+    slider_area();
+
+    /*================================
+    Fullscreen Page
+    ==================================*/
+
+    if ($('#full-view').length) {
+
+        var requestFullscreen = function(ele) {
+            if (ele.requestFullscreen) {
+                ele.requestFullscreen();
+            } else if (ele.webkitRequestFullscreen) {
+                ele.webkitRequestFullscreen();
+            } else if (ele.mozRequestFullScreen) {
+                ele.mozRequestFullScreen();
+            } else if (ele.msRequestFullscreen) {
+                ele.msRequestFullscreen();
+            } else {
+                console.log('Fullscreen API is not supported.');
+            }
+        };
+
+        var exitFullscreen = function() {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else {
+                console.log('Fullscreen API is not supported.');
+            }
+        };
+
+        var fsDocButton = document.getElementById('full-view');
+        var fsExitDocButton = document.getElementById('full-view-exit');
+
+        fsDocButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            requestFullscreen(document.documentElement);
+            $('body').addClass('expanded');
+        });
+
+        fsExitDocButton.addEventListener('click', function(e) {
+            e.preventDefault();
+            exitFullscreen();
+            $('body').removeClass('expanded');
+        });
+    }
+
+})(jQuery);
+
+
+
+
+// * Profile Modal
+
+const profileBtn = document.getElementById('profileModal');
+const showModalBtn = document.getElementById('show_modal_btn');
+const profileModalActive = document.querySelector('.profile-Modal_active');
+
+showModalBtn.addEventListener('click', (e) => {
+   profileBtn.classList.toggle('profile-modal_active'); 
+});
+
+// * Client New Request Multisection from 
+
+ const steps = [...document.querySelectorAll('div .step')]; 
+ const nextBtn = document.querySelectorAll('.next-btn');
+ const prevBtn = document.querySelectorAll('prev-btn');
+ const clientRequestForm = document.querySelector('.client-newrequest');
+
+ if(clientRequestForm) {
+    clientRequestForm.addEventListener('click', (e) => {
+        e.preventDefault();
+     })
+ }
+ 
+ nextBtn.forEach(button => {
+     button.addEventListener('click', (e) => {
+         changeStep('next');
+         clientRequestForm.preventDefault()
+
+     })
+ })
+
+ prevBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+        changeStep('prev');
+        
+     })
+ })
+
+ function changeStep(btn) {
+     let index = 0;
+     const active = document.querySelector('.step.active');
+     index = steps.indexOf(active);
+     steps[index].classList.remove('active');
+     if(btn === 'next') {
+         index++;
+     }else if(btn === 'prev'){
+         index--;
+     }
+     steps[index].classList.add('active');
+ }
+
+   // * For the Sidbar to side in and out
+    
+    const hamburgerMenu = document.getElementById('sidebar-menu');
+    const sidebar = document.querySelector('.sidebar-menu');
+    const mainPageContent = document.querySelector('.main-pagecontent');
+    const biggerMarginDiv = [...document.querySelectorAll('#bigger-margin')];
+
+    if(hamburgerMenu) {
+        hamburgerMenu.addEventListener('click', (e) =>{
+            alternatePage()
+        });
+    }
+    
+    function alternatePage() {
+        sidebar.classList.toggle('sidebar-menu-active');
+        mainPageContent.classList.toggle('main-pagecontent-active');
+        biggerMarginDiv.forEach(div => {
+            div.classList.toggle('ml-7')
+        });
+    }
+
+    // * Description Carousel
+
+    const descTab = document.getElementById('descTab');
+    const devTab = document.getElementById('devTab');
+    const attTab = document.getElementById('attTab');
+    const jobListBtn = document.querySelector('.job-list-details-active');
+
+    const descContent = document.getElementById('descriptionContent');
+    const devContent = document.getElementById('delivarablesContent');
+    const attContent = document.getElementById('attachmentContent');
+
+    descTab.onclick = () =>{
+        descContent.style.display = 'block';
+        devContent.style.display = 'none';
+        attContent.style.display = 'none';
+    }
+    devTab.onclick = () =>{
+        descContent.style.display = 'none';
+        devContent.style.display = 'block';
+        attContent.style.display = 'none';
+    }
+    attTab.onclick = () =>{
+        descContent.style.display = 'none';
+        devContent.style.display = 'none';
+        attContent.style.display = 'block';
+    }
+
+
--- src/main/resources/static/assets/JS/onboarding.js
+++ src/main/resources/static/assets/JS/onboarding.js
@@ -1,178 +1,187 @@
-    const freelanceBox = document.getElementById("Freelance-box")
-    const clientBox = document.getElementById("client-box")
-    
-    
-    if(freelanceBox) {
-        freelanceBox.addEventListener("mouseover", (e) => {
-            document.querySelector('#Freelancer-icon').classList.add('Freelancer-icon')
-            
-           })
-           freelanceBox.addEventListener("mouseout", (e) => {
-            document.querySelector('#Freelancer-icon').classList.remove('Freelancer-icon')
-            
-           })
-        freelanceBox.addEventListener('click', (e) => {
-            document.getElementById("btn-normal").style.display = 'none';
-            document.getElementById("btn-freelancer").style.display = 'inline-block';
-            document.getElementById("btn-client").style.display = 'none';
-        })
-      
-    }
-   
-    if(clientBox) {
-        clientBox.addEventListener("mouseover", (e) => {
-            document.querySelector('#client-icon').classList.add('client-icon')
-            
-           })
-           clientBox.addEventListener("mouseout", (e) => {
-            document.querySelector('#client-icon').classList.remove('client-icon')
-            
-           })
+const freelanceBox = document.getElementById('Freelance-box');
+const clientBox = document.getElementById('client-box');
+const clientIcon = document.querySelector('#client-icon');
+const btn = document.querySelector('.lol');
+const signModal = document.querySelector('.sign-modal');
+const freelancerIcon = document.querySelector('#Freelancer-icon');
 
-        clientBox.addEventListener('click', (e) => {
-            document.getElementById("btn-normal").style.display = 'none';
-            document.getElementById("btn-freelancer").style.display = 'none';
-            document.getElementById("btn-client").style.display = 'inline-block'; 
-        })
-    }
-   
-//Movement of Freelancer's form 
+// FREELANCER BOX
+if (btn)
+  btn.addEventListener('click', () => (signModal.style.display = 'none'));
+if (freelanceBox) {
+  freelanceBox.addEventListener('mouseover', () =>
+    freelancerIcon.classList.add('Freelancer-icon')
+  );
+  freelanceBox.addEventListener('mouseleave', () =>
+    freelancerIcon.classList.remove('Freelancer-icon')
+  );
+  freelanceBox.addEventListener('click', (e) => {
+    document.getElementById('btn-normal').style.display = 'none';
+    freelancerIcon.classList.add('Freelancer-icon');
+    clientIcon.classList.remove('client-icon');
+    freelanceBox.style.border = '1px solid #151617';
+    clientBox.style.border = 'none';
+    document.getElementById('btn-freelancer').style.display = 'inline-block';
+    document.getElementById('btn-client').style.display = 'none';
+  });
+}
 
-const allFreelancerFrom = [...document.querySelectorAll(".step")] // used the spread operator to convert the Nodelist from the queryselectorAll to an Array
-const formBtnNext = document.querySelectorAll("#formBtn-next");
-const formBtnPrev = document.querySelectorAll("#formBtn-prev")
+// CLIENT BOX
+if (clientBox) {
+  clientBox.addEventListener('mouseover', () =>
+    clientIcon.classList.add('client-icon')
+  );
+  clientBox.addEventListener('mouseleave', () =>
+    clientIcon.classList.remove('client-icon')
+  );
+  clientBox.addEventListener('click', (e) => {
+    clientIcon.classList.add('client-icon');
+    clientBox.style.border = '1px solid #151617';
+    freelancerIcon.classList.remove('Freelancer-icon');
+    freelanceBox.style.border = 'none';
+    document.getElementById('btn-normal').style.display = 'none';
+    document.getElementById('btn-freelancer').style.display = 'none';
+    document.getElementById('btn-client').style.display = 'inline-block';
+  });
+}
+
+//Movement of Freelancer's form
+const allFreelancerFrom = document.querySelectorAll('.step');
+const formBtnNext = document.querySelectorAll('#formBtn-next');
+const formBtnPrev = document.querySelectorAll('#formBtn-prev');
 const mainForm = document.querySelector('#mainform');
 
-formBtnNext.forEach(button => {
-    button.addEventListener("click", (e) => {
-        changeStep('next');
-    })
-})
-formBtnPrev.forEach(button => {
-    button.addEventListener("click", (e) => {
-        changeStep('prev');
-    })
-})
+formBtnNext.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    changeStep('next');
+  });
+});
+formBtnPrev.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    changeStep('prev');
+  });
+});
+
+let i = 0;
 
 function changeStep(btn) {
-    let index = 0;
-    const active = document.querySelector(".active");
-    index = allFreelancerFrom.indexOf(active)
-    allFreelancerFrom[index].classList.remove('active');
-    if(btn === 'next') {
-        index++;
-    }
-    else if (btn === 'prev') {
-        index--;
-    }
-    allFreelancerFrom[index].classList.add('active');
+  if (btn === 'next') {
+    i++;
+    allFreelancerFrom[i].style.display = 'block';
 
+    allFreelancerFrom[i - 1].style.display = 'none';
+  } else if (btn === 'prev') {
+    if (i > 0) {
+      i--;
+      allFreelancerFrom[i].style.display = 'block';
+      allFreelancerFrom[i + 1].style.display = 'none';
+    }
+  }
 }
 
- //The multi section form
-const cardForm = document.getElementById("cardform");
-const cards = [...document.querySelectorAll(".multistep")];
-const cardBtnNext = document.querySelectorAll("#btn-next");
-const cardBtnPrev = document.querySelectorAll("#btn-prev");
-const formProgressBox = [...document.querySelectorAll(".form-progress-box")]
-const formProgressText = [...document.querySelectorAll(".form-progress-text")]
+//The multi section form
+const cardForm = document.getElementById('cardform');
+const cards = document.querySelectorAll('.multistep');
+const cardBtnNext = document.querySelectorAll('#btn-next');
+const cardBtnPrev = document.querySelectorAll('#btn-prev');
+const formProgressBox = [...document.querySelectorAll('.form-progress-box')];
+const formProgressText = [...document.querySelectorAll('.form-progress-text')];
 
-cardBtnNext.forEach(button => {
-    button.addEventListener("click", (e) => {
-        moveCard('next');
-    })
-})
+if (cardBtnNext) {
+  cardBtnNext.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      moveCard('next');
+    });
+  });
+}
 
-cardBtnPrev.forEach(button => {
-    button.addEventListener("click", (e) => {
-        moveCard('prev')
-    })
-})
-
+if (cardBtnPrev) {
+  cardBtnPrev.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      moveCard('prev');
+    });
+  });
+}
+// cards[0].style.display = 'block';
+let index = 0;
+let progress = 20;
+const progressionBar = document.querySelector('.progress__bar--bar');
 function moveCard(btn) {
-    let index = 0;
-    let progress = 0;
-    let textProgress = 0;
-    const activeprogressbox = document.querySelector(".form-progress-active")
-    const activeProgressText= document.querySelector(".form-progress-text-active")
-    const activeCard = document.querySelector(".active-card");
+  if (btn === 'next') {
+    index++;
+    progress = progress + 20;
+    progressionBar.style.width = `${progress}%`;
+    cards[index].style.display = 'block';
 
-    index = cards.indexOf(activeCard);
-    progress = formProgressBox.indexOf(activeprogressbox)
-    textProgress = formProgressText.indexOf(activeProgressText)
-
-    formProgressBox[progress].classList.remove('form-progress-active')
-    formProgressText[textProgress].classList.remove('form-progress-text-active')
-    cards[index].classList.remove('active-card');
-    if(btn === 'next') {
-        index++;
-        progress++;
-        textProgress++;
-    }else if(btn === 'prev'){
-        index--;
-        progress--;
-        textProgress--;
+    cards[index - 1].style.display = 'none';
+  } else if (btn === 'prev') {
+    if (index > 0) {
+      index--;
+      cards[index].style.display = 'block';
+      cards[index + 1].style.display = 'none';
+      if (progress > 19) {
+        progress = progress - 20;
+        progressionBar.style.width = `${progress}%`;
+      }
     }
-    cards[index].classList.add('active-card');
-    formProgressBox[progress].classList.add('form-progress-active')
-    formProgressText[textProgress].classList.add('form-progress-text-active')
-}
+  }
 
+  // cards
+}
 
 //Client image upload
-const bgImg =document.querySelector(".client-upload") // the bg where the uploaded profile would stay
-const img_input = document.querySelector("#profile-picture") // the input type of file
+const bgImg = document.querySelector('.client-upload'); // the bg where the uploaded profile would stay
+const img_input = document.querySelector('#profile-picture'); // the input type of file
 
-    if(img_input) {
-        img_input.onchange = (e) => { // so when the input experiences a change that it, it has clicked
-            let file = e.target.files[0] // the file variable here holds the image you selected from your file explorer
-            let url = URL.createObjectURL(file); // the createObjectURL is a method used to create for the specified object which in our case is a file, so the url variable holds the url to this file(image) we have selected
-            bgImg.style.background = `url(${url}) center / cover` // we select the bg that holds the image and set its CSS bg to the url that we have from the url table
-            bgImg.querySelector('.profile-picture').style.display = "none"; // then we remove the person icon that is on the background
-        }
-    }
-   
-
-
-
+if (img_input) {
+  img_input.onchange = (e) => {
+    // so when the input experiences a change that it, it has clicked
+    let file = e.target.files[0]; // the file variable here holds the image you selected from your file explorer
+    let url = URL.createObjectURL(file); // the createObjectURL is a method used to create for the specified object which in our case is a file, so the url variable holds the url to this file(image) we have selected
+    bgImg.style.background = `url(${url}) center / cover`; // we select the bg that holds the image and set its CSS bg to the url that we have from the url table
+    bgImg.querySelector('.profile-picture').style.display = 'none'; // then we remove the person icon that is on the background
+  };
+}
 
 // The Accordion with a form element
 
-const serviceBtn = document.querySelectorAll(".services-head") // the accordion element
+const servicesBtn = document.querySelectorAll('.services-head');
+const serviceBtn = document.querySelector('.services-head');
+const editIcon = document.querySelector('.edit-icon');
 
-    if(serviceBtn) {
-        serviceBtn.forEach(button => {
-            button.addEventListener("click", () => {
-                const serviceContent = button.nextElementSibling;
-                button.classList.toggle('service-content-active');
+// The accordion element
+if (serviceBtn) {
+  servicesBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      editIcon.style.opacity = 1;
+      const serviceContent = button.nextElementSibling;
+      button.classList.toggle('service-content-active');
 
-                if(button.classList.contains('service-content-active')) {
-                    serviceContent.style.maxHeight = serviceContent.scrollHeight + 'px';
-                } else {
-                    serviceContent.style.maxHeight = 0;
-                }
-            });
-        })
-       
-    }
-
+      if (button.classList.contains('service-content-active')) {
+        serviceContent.style.maxHeight = serviceContent.scrollHeight + 'px';
+        console.log(serviceContent.scrollHeight);
+      } else {
+        serviceContent.style.maxHeight = 0;
+      }
+    });
+  });
+}
+const selectyear = document.querySelector('.open-pop');
+if (selectyear) {
+  selectyear.addEventListener('click', () => {
+    document.getElementById('popupI').style.display = 'block';
+  });
+}
 // Custom file upload function
-const uploadBtn = [...document.querySelectorAll('#upload')];
-const uploadBtn1 = document.getElementById('#upload-1');
+// const uploadBtn = [...document.querySelectorAll('#upload')];
+// const uploadBtn1 = document.getElementById('#upload-1');
 
-const uploadText = [...document.querySelectorAll('#custom-file-upload-text')];
-
-
-// uploadBtn[0].addEventListener('change', (e) => {
+// const uploadText = [...document.querySelectorAll('#custom-file-upload-text')];
+// if (uploadBtn) {
+//   uploadBtn[0].addEventListener('change', (e) => {
 //     uploadText[0].innerText = e.target.files[0].name;
-// })
-uploadBtn[1].addEventListener('change', (e) => {
-    uploadText[1].innerText = e.target.files[0].name;
-})
-
-
-
-
-
-
-
+//   });
+//   uploadBtn[1].addEventListener('change', (e) => {
+//     uploadText[1].innerText = e.target.files[0].name;
+//   });
+// }
